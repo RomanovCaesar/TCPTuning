ce8112216ea17fcc641df3bea862237885a0c197
Romanov Caesar <88546123+RomanovCaesar@users.noreply.github.com>
2025-01-23 12:18:44 +0800

    Create jptcp.sh

---
+++ jptcp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 设置TCP缓冲区大小
+sysctl -w net.ipv4.tcp_wmem="4096 16384 1875000"
+sysctl -w net.ipv4.tcp_rmem="4096 87380 1875000"
+
+# 将设置写入/etc/sysctl.conf以便永久保存
+echo "net.ipv4.tcp_wmem = 4096 16384 1875000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 1875000" >> /etc/sysctl.conf
+
+# 应用更改
+sysctl -p
